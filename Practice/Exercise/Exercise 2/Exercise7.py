e7eb579aff49b835249dfe9a066bca5836b75f15
Mehul Adhikari <155364569+m0nteaaaaa@users.noreply.github.com>
2025-01-23 18:32:33 +0530

    Add files via upload

---
+++ Practice/Exercise/Exercise 2/Exercise7.py
@@ -0,0 +1,4 @@
+a = input('Enter name ')
+b = int(input('Enter age'))
+
+print(a, 'You will be ' , b+10 ,' years old after 10 Years')
---
+++ Practice/Exercise/Exercise 2/Exercise_5.py
@@ -0,0 +1,13 @@
+# Exercise Functions
+# Highest Even: Write a function to find the highest even number from the list.
+
+
+def highest_even(li):
+    evens = []
+    for item in li:
+        if item %2==0:
+            evens.append(item)
+    return max(evens)
+
+
+print(highest_even([10, 2, 3, 4, 8, 11]))
